3c4534fd3a7ae04948b41c951422d6b9e1f81de2
EmanuelQ3 <emanuel.quezada@hotmail.com>
2024-11-07 03:43:52 +0000

    added the JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+let animal= "Dragon"
+console.log("If you're happy and you know it!")
+console.log("Hello my friend!")
+console.log(animal)
+let favoriteFood = "BBQ"
+console.log(favoriteFood)
+favoriteFood = "Grapes"
+console.log(favoriteFood)
+const favoriteHoliday = "Easter"
+console.log(favoriteHoliday)
+favoriteHoliday = "Halloween"
